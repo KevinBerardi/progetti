0c8ef06bb9713a5aa90d756125269a51232ede02
Kevin <144828368+KevinBerardi@users.noreply.github.com>
2023-10-27 17:33:58 +0200

    Add files via upload

---
+++ loading.js
@@ -0,0 +1,15 @@
+const indicator = document.querySelector("indicator");
+        const result = document.querySelector("output");
+
+        var percent = 0;
+        var interval;
+
+        interval = setInterval (function() {
+            percent++;
+            output.value = percent + "%";
+            indicator.setAttribute("style", `--completion: ${percent}%`);
+
+            if (percent == 100){
+                clearInterval(interval);
+            }
+        }, 60);
